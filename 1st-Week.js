782d0acdcc6a4433d8987a36114003c7ce7abadf
Mohd Khalid <er.khalidmohd@gmail.com>
2026-03-20 15:58:19 +0530

    Q-8 Solved

--- 1st-Week.js
+++ 1st-Week.js
@@ -57,3 +57,11 @@ function calculateRectangle(length, width){
 
 calculateRectangle(15, 20)
 
+// Accept the parameters and calculate the Compound Interest & print it on STDOUT (UseMath class methods)
+
+function compoundInteres(p, r, t){
+ let amount = p*(Math.pow(1+(r/100),t))
+ let ci = amount - p
+ console.log(`CI = ${ci}`)
+}
+compoundInteres(10000, 5, 2)
