828a4de4a0426e6b8b8df5509b5c817ef2d52dd7
Mohd Khalid <er.khalidmohd@gmail.com>
2026-04-13 02:20:43 +0530

    Add implementation for counting operations to obtain zero; create new file for Q-29 with problem statement.

--- 2nd-Week.js
+++ 2nd-Week.js
@@ -454,3 +454,11 @@ for(let i = 0; i < arr.length; i++){
    }
 }
 console.log(result)
+
+
+
+
+// Count Operations to Obtain Zero
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let max = Math.max(...arr);
+console.log(max)
