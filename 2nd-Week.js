1cae93de2540ec7bb7399eb4bf36c3ea9e9d2c3a
Mohd Khalid <er.khalidmohd@gmail.com>
2026-04-14 22:51:43 +0530

    Add implementation for finding the greatest common divisor of an array; remove unnecessary whitespace and comments.

--- 2nd-Week.js
+++ 2nd-Week.js
@@ -385,7 +385,6 @@ console.log(result)
 
 
 
-
 // Divide Array Into Equal Pairs
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let set = new Set()
@@ -457,7 +456,6 @@ console.log(result)
 
 
 
-
 // Count Operations to Obtain Zero
 let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let max = Math.max(...arr);
@@ -493,3 +491,16 @@ console.log(count)
 
 
 // Find Greatest Common Divisor of Array
+let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function num(a, b){
+   while(b !== 0){
+     let temp = b;
+     b = a % b;
+     a = temp;
+   }
+   return a
+}
+let min =  Math.min(...arr);
+let max = Math.max(...arr);
+let result = num(min, max);
+console.log(result);
